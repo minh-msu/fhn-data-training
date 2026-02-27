df1affcf3069bd035adfc9e8cda6b2f8ad0ab6cb
minh-msu <166856749+minh-msu@users.noreply.github.com>
2026-02-27 15:58:26 +0700

    Update tune.py

--- src/tune.py
+++ src/tune.py
@@ -7,13 +7,16 @@ from xgboost import XGBRegressor
 import sklearn
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error
+
 from utils import *
 
 def main():
     df = next(
         pd.read_csv("data/train.csv", chunksize=100000)
     )
-    X, y = transform("../data/train.csv")
+    X, y = transform(df, "fare_amount")
     features = X.columns.tolist()
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, 
@@ -23,17 +26,10 @@ def main():
     param_grid = {
         "n_estimators": [100, 300, 500],          # number of boosting rounds
         "max_depth": [3, 5, 7, 9],                # tree depth
-        "learning_rate": [0ta.01, 0.05, 0.1, 0.2],  # shrinkage step size
+        "learning_rate": [0.01, 0.05, 0.1, 0.2]  # shrinkage step size
     }
-    #     "subsample": [0.6, 0.8, 1.0],             # row sampling
-    #     "colsample_bytree": [0.6, 0.8, 1.0],      # feature sampling
-    #     "min_child_weight": [1, 3, 5],            # minimum sum of instance weight (hessian) needed in a child
-    #     "gamma": [0, 0.1, 0.2, 0.5],              # minimum loss reduction required for a split
-    #     "reg_alpha": [0, 0.1, 1],                 # L1 regularization
-    #     "reg_lambda": [1, 5, 10]                  # L2 regularization
-    # }
     grid = GridSearchCV(
-        estimator=model, 
+        estimator=XGBRegressor(), 
         param_grid=param_grid, 
         scoring="neg_root_mean_squared_error",
         cv=cv, 
